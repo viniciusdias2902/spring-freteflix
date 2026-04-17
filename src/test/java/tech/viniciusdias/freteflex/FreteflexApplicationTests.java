f51f2e0a16bb41bcb5d3208c919db5e6e07ca062
Vinicius Dias <vyvy292004@gmail.com>
2026-04-17 19:00:54 -0300

    initialize Spring Boot project for shipping cost calculation

---
+++ src/test/java/tech/viniciusdias/freteflex/FreteflexApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.viniciusdias.freteflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
