d7405d10a9ab247d15113cd1046263ac7ba460ad
Vinicius Dias <vyvy292004@gmail.com>
2026-04-17 19:01:04 -0300

    add GET /shipping/calculate endpoint

---
+++ src/main/java/tech/viniciusdias/freteflex/controller/ShippingController.java
@@ -0,0 +1,28 @@
+package tech.viniciusdias.freteflex.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tech.viniciusdias.freteflex.controller.dto.ShippingResponse;
+import tech.viniciusdias.freteflex.service.ShippingService;
+
+@RestController()
+@RequestMapping("/shipping")
+public class ShippingController {
+    private final ShippingService shippingService;
+
+    public ShippingController(ShippingService shippingService) {
+        this.shippingService = shippingService;
+    }
+
+    @GetMapping(value = "/calculate")
+    public ResponseEntity<ShippingResponse> calculte(@RequestParam String shippingType,
+                                                     @RequestParam Double distance,
+                                                     @RequestParam Double weight) {
+        Double cost = shippingService.calculate(shippingType, distance, weight);
+        return ResponseEntity.ok(new ShippingResponse(cost));
+    }
+}
